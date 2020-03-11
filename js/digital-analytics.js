e8f3b10992d62e7f42188da0a2d2ac875ebbc934
DigitalAnalytics2020 <61888243+DigitalAnalytics2020@users.noreply.github.com>
2020-03-11 16:27:38 -0400

    Update digital-analytics.js

--- js/digital-analytics.js
+++ js/digital-analytics.js
@@ -1,3 +1,32 @@
-var CWC_MLCA = CWC_MLCA || {};
-CWC_MLCA.dataLayer = {};
-CWC_MLCA.dataLayer.debug = "true";
+var CWC_MLCA = CWC_MLCA || {
+
+  // Initialize dataLayer
+  // set "debug" to "true" in development environment
+  // set "debug" to "false" in production environment
+  dataLayer: {
+    "debug": "true"
+  },
+
+  // Set Site Information
+  site: function() {
+    this.dataLayer.site = {
+      "brand": "Test Co.", 
+      "name": "test", 
+      "type": "desktop", 
+      "environment": "development", 
+      "version": "0.0.1", 
+      "lastBuildDate": "2020-02-02" // need standardize format
+    }
+  },
+
+  // Set Page Information
+  page: function(name, title, category, language) {
+    this.dataLayer.page = {
+      "name": name, 
+      "title": title, 
+      "category": category, 
+      "language": language 
+    }
+  }
+
+};
