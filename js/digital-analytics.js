8a9b385539e6846d4de5d9abe5ad89676059c152
DigitalAnalytics2020 <61888243+DigitalAnalytics2020@users.noreply.github.com>
2020-03-20 12:16:33 -0400

    Update digital-analytics.js

--- js/digital-analytics.js
+++ js/digital-analytics.js
@@ -29,6 +29,20 @@ var CWC_MLCA = CWC_MLCA || {
     }
   },
 
+  // Push Product Information
+  product: function(name, productID, category, group, coverage, value, tipInfo) {
+    this.dataLayer.products = this.dataLayer.products || [];
+    this.dataLayer.products.push({
+      "name": name,
+      "productID": productID, 
+      "category": category,
+      "group": group,
+      "coverage": coverage,
+      "value": value,
+      "tipInfo": tipInfo
+    });
+  },
+
   // Push Form Information
   form: function(name, appID, step, type) {
     this.dataLayer.apps = this.dataLayer.apps || [];
@@ -48,6 +62,53 @@ var CWC_MLCA = CWC_MLCA || {
       "category": category,
       "name": name,
       "detail": detail
+    });
+  },
+
+  // Set Transaction Information
+  transaction: function(transactionID, userID, mount, category, product) {
+    this.dataLayer.transaction = {
+      "transactionID": transactionID, 
+      "userID": userID, 
+      "mount": mount, 
+      "category": category, 
+      "product": product 
+    }
+  },
+
+  // Push Component Information
+  component: function(name, componentID, description) {
+    this.dataLayer.components = this.dataLayer.components || [];
+    this.dataLayer.components.push({
+      "name": name,
+      "componentID": componentID, 
+      "description": description
+    });
+  },
+
+  // Push User Information
+  user: function(userID, userRole, userGroup, advisorID, status) {
+    this.dataLayer.users = this.dataLayer.users || [];
+    this.dataLayer.users.push({
+      "userID": userID,
+      "userRole": userRole,
+      "userGroup": userGroup,
+      "advisorID": advisorID, 
+      "status": status
+    });
+  },
+
+  // Push Customer Information
+  customer: function(customerStatus, gender, age, smokeStatus, province, profile, employment) {
+    this.dataLayer.customers = this.dataLayer.customers || [];
+    this.dataLayer.customers.push({
+      "customerStatus": customerStatus,
+      "gender": gender,
+      "age": age,
+      "smokeStatus": smokeStatus,
+      "province": province,
+      "profile": profile, 
+      "employment": employment
     });
   }
 
