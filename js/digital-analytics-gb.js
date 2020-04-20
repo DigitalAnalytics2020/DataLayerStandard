043a72d1ee60f992b2d34478f0898976e8f97ffb
DigitalAnalytics2020 <61888243+DigitalAnalytics2020@users.noreply.github.com>
2020-04-20 11:29:52 -0400

    Update digital-analytics-gb.js

--- js/digital-analytics-gb.js
+++ js/digital-analytics-gb.js
@@ -13,6 +13,19 @@ function gbProduct(name, productId, category, group, coverage, value, tipInfo) {
   );
 }
 
+// Customized transaction data for GB project
+function gbTransaction(transactionId, userId, mount, category, product) {
+  window.CWC_MLCA.transaction(
+    {
+      "transactionId": transactionId, 
+      "userId": userId, 
+      "mount": mount, 
+      "category": category, 
+      "product": product 
+    }
+  );
+}
+
 // Customized customer data for GB project
 function gbCustomer(customerStatus, gender, age, smokeStatus, province, profile, employment) {
   window.CWC_MLCA.customer(
