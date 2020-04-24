6d35e090f4fa8fd8dab8791821a7e700d82a336a
DigitalAnalytics2020 <61888243+DigitalAnalytics2020@users.noreply.github.com>
2020-04-24 13:31:24 -0400

    Update digital-analytics-gb.js

--- js/digital-analytics-gb.js
+++ js/digital-analytics-gb.js
@@ -1,42 +1,46 @@
-// Customized products data for GB project
-function gbProduct(name, productId, category, group, coverage, value, tipInfo) {
-  window.CWC_MLCA.product(
-    [{
-      "name": name,
-      "productId": productId, 
-      "category": category,
-      "group": group,
-      "coverage": coverage,
-      "value": value,
-      "tipInfo": tipInfo
-    }]
-  );
-}
+var CWC_MLCA_GB = CWC_MLCA_GB || {
 
-// Customized transaction data for GB project
-function gbTransaction(transactionId, userId, mount, category, product) {
-  window.CWC_MLCA.transaction(
-    {
-      "transactionId": transactionId, 
-      "userId": userId, 
-      "mount": mount, 
-      "category": category, 
-      "product": product 
-    }
-  );
-}
+  // Customized products data for GB project
+  product: function(name, productId, category, group, coverage, value, tipInfo) {
+    window.CWC_MLCA.product(
+      [{
+        "name": name,
+        "productId": productId, 
+        "category": category,
+        "group": group,
+        "coverage": coverage,
+        "value": value,
+        "tipInfo": tipInfo
+      }]
+    );
+  },
 
-// Customized customer data for GB project
-function gbCustomer(customerStatus, gender, age, smokeStatus, province, profile, employment) {
-  window.CWC_MLCA.customer(
-    {
-      "customerStatus": customerStatus,
-      "gender": gender,
-      "age": age,
-      "smokeStatus": smokeStatus,
-      "province": province,
-      "profile": profile, 
-      "employment": employment
-    }
-  );
+  // Customized transaction data for GB project
+  transaction: function(transactionId, userId, mount, category, product) {
+    window.CWC_MLCA.transaction(
+      {
+        "transactionId": transactionId, 
+        "userId": userId, 
+        "mount": mount, 
+        "category": category, 
+        "product": product 
+      }
+    );
+  },
+
+  // Customized customer data for GB project
+  customer: function(customerStatus, gender, age, smokeStatus, province, profile, employment) {
+    window.CWC_MLCA.customer(
+      {
+        "customerStatus": customerStatus,
+        "gender": gender,
+        "age": age,
+        "smokeStatus": smokeStatus,
+        "province": province,
+        "profile": profile, 
+        "employment": employment
+      }
+    );
+  }
+
 }
